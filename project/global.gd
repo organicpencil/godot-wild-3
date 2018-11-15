fc76a794e7d209148b2df282bfb79869109d3caf
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-11-14 18:48:28 -0600

    Add music

--- project/global.gd
+++ project/global.gd
@@ -1,5 +1,12 @@
 extends Node
 
+func _ready():
+	# Deferred rather than autoloaded, prevents music from playing during the initial load
+	call_deferred("add_music_player")
+	
+func add_music_player():
+	add_child(load("res://music_player.tscn").instance())
+
 func _input(event):
 	if event.is_action_pressed("toggle_fullscreen"):
 		OS.window_fullscreen = !OS.window_fullscreen
