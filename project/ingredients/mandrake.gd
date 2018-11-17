5bb9a16786252bea3544b3521afef0b320f2ea76
Lee Pugh <4343398+organicpencil@users.noreply.github.com>
2018-11-17 15:46:58 -0600

    Mandrake expressions

--- project/ingredients/mandrake.gd
+++ project/ingredients/mandrake.gd
@@ -1,5 +1,14 @@
 extends "res://ingredients/ingredient.gd"
 
+const TEXTURE_SAD = preload("res://ingredients/mandrake.png")
+const TEXTURE_SAD_HOVER = preload("res://ingredients/mandrake_hoover.png")
+
 func _init():
 	ingredient_id = Global.ING_MANDRAKE
 	mix_color = Color(0, 1, 0, 1)
+	
+func handle_button_pressed():
+	.handle_button_pressed()
+	if !active_mode:
+		$TextureButton.texture_normal = TEXTURE_SAD
+		$TextureButton.texture_hover = TEXTURE_SAD_HOVER
