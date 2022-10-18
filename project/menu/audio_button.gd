1bef23bd2b1460f19b1451346c640abaa869c590
organicpencil <4343398+organicpencil@users.noreply.github.com>
2022-10-18 15:28:09 -0500

    Fix audio button issues, new audio button icon

--- project/menu/audio_button.gd
+++ project/menu/audio_button.gd
@@ -1,15 +1,8 @@
-extends CheckButton
+extends TextureButton
 
-export var start_muted = false # For testing
 
 func _ready():
-	if start_muted:
-		AudioServer.set_bus_mute(0, true)
-
-	connect("visibility_changed", self, "update_status")
-
-func update_status():
 	pressed = !AudioServer.is_bus_mute(0)
 
-func _toggled(button_pressed):
-	AudioServer.set_bus_mute(0, !AudioServer.is_bus_mute(0))
+func _toggled(button_pressed: bool):
+	AudioServer.set_bus_mute(0, !button_pressed)
